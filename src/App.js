c6e5ce1922a2a14454ca3c5ad8d0d2df00972493
Alex Lévy <alexlevy0@gmail.com>
2023-12-08 21:03:53 +0100

    Go

--- src/App.js
+++ src/App.js
@@ -1,58 +1,75 @@
-import React, { Suspense, useRef, useEffect } from 'react'
-import * as THREE from 'three'
-import { PositionalAudio, OrbitControls, CameraShake } from '@react-three/drei'
-import { useControls } from 'leva'
-import { Particles } from './Particles'
-import { SpeedInsights } from '@vercel/speed-insights/react'
-import { Analytics } from '@vercel/analytics/react'
+import { CameraShake, OrbitControls, PositionalAudio } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
+import { useControls } from 'leva'
+import React, { useEffect, useRef, useState } from 'react'
+import * as THREE from 'three'
+import { Particles } from './Particles'
 
 export default function App() {
     const sound = useRef()
     const analyser = useRef()
     const renderRef = useRef()
-    const audioMooves = useRef(0.25)
+    const [link, setLink] = useState('')
+
+    useEffect(
+        () =>
+            void (analyser.current = new THREE.AudioAnalyser(
+                sound.current,
+                2048,
+            )),
+        [sound],
+    )
 
     useEffect(() => {
-        // 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768. Defaults to 2048
-        return void (analyser.current = new THREE.AudioAnalyser(sound.current, /*32  */ 2048))
-    }, [sound])
+        console.log('pasted or dropped :', link)
+        // setLink('')
+    }, [link])
 
     useEffect(() => {
+        document.addEventListener('paste', e => {
+            if (e.clipboardData.types.indexOf('text/plain') === -1) return
+            e.preventDefault()
+            const link = e.clipboardData.getData('text/plain')
+            if (!link) return
+            setLink(link)
+        })
         setTimeout(() => {
-            console.log('play1', sound.current)
-            if (sound.current) {
-                console.log('play2')
-                sound.current.play()
-            }
+            if (!sound.current) return
+            sound.current.play()
         }, 3000)
         return () => sound.current.pause()
     }, [])
 
-    const ratioL = 250
-
     useFrame(() => {
-        if (analyser.current && sound.current && renderRef.current) {
-            const frequencyData = analyser.current.getFrequencyData().filter(Number)
-            const size = frequencyData.length
-            if (!size) return
-
-            const part = size / 4
-            const lowF = frequencyData.slice(-part)
-            const lowV = lowF.reduce((acc, val) => acc + val, 0) / lowF.length
-            const lValue = lowV / ratioL
-            // console.log('->', lValue)
-            renderRef.current.uniforms.audioMooves.value = lValue ? lValue : 0.25
-        }
+        if (!analyser.current || !sound.current || !renderRef.current) return
+        const frequencyData = analyser.current.getFrequencyData().filter(Number)
+        const size = frequencyData.length
+        if (!size) return
+        const part = size / (props.Hz || 4)
+        const lowF = frequencyData.slice(-part)
+        const lowV = lowF.reduce((ac, v) => ac + v, 0) / lowF.length
+        const lValue = lowV / (props.tuner || 250)
+        renderRef.current.uniforms.audioMooves.value = lValue ? lValue : 0.25
     })
 
-    const props = useControls({
-        focus: { value: 5.1, min: 3, max: 7, step: 0.01 },
-        speed: { value: 30, min: 0.1, max: 100, step: 0.1 },
-        aperture: { value: 1, min: 1, max: 5.6, step: 0.1 },
-        fov: { value: 50, min: 0, max: 200 },
-        curl: { value: 0.25, min: 0.01, max: 1, step: 0.01 },
-    })
+    const props = useControls(
+        ' ',
+        {
+            focus: { value: 5.1, min: 3, max: 7, step: 0.01 },
+            speed: { value: 30, min: 0.1, max: 100, step: 0.1 },
+            aperture: { value: 1, min: 1, max: 5.6, step: 0.1 },
+            fov: { value: 50, min: 0, max: 200 },
+            // curl: { value: 0.25, min: 0.01, max: 1, step: 0.01 },
+            'Low Hz': { value: 4, min: 1, max: 8, step: 0.5 },
+            Response: { value: 250, min: 1, max: 500, step: 50 },
+        },
+        {
+            collapsed: true,
+        },
+    )
+
     return (
         <>
             <OrbitControls
@@ -75,11 +92,43 @@ export default function App() {
                 renderRef={renderRef}
             />
             <PositionalAudio
-                url={'sounds/A.mp3'}
+                url={'sounds/topdown.mp3'}
                 ref={sound}
             />
+            <DragAndDrop setLink={setLink} />
             <SpeedInsights />
             <Analytics />
         </>
     )
+}
+
+const DragAndDrop = ({ setLink }) => {
+    const enter = e => {
+        console.log('enter')
+        e.currentTarget.classList.add('dragging')
+        e.dataTransfer.clearData()
+        e.dataTransfer.setData('text/uri-list', e.target.id)
+        e.dataTransfer.effectAllowed = 'move'
+    }
+    const over = e => e.preventDefault() || (e.dataTransfer.dropEffect = 'move')
+    const drop = e => {
+        e.preventDefault()
+        e.dataTransfer.dropEffect = 'move'
+        e.dataTransfer.effectAllowed = 'copyLink'
+        e.currentTarget.classList.remove('dragging')
+        const data = e.dataTransfer.getData('text/uri-list')
+        if (!data) return
+        setLink(data)
+    }
+    useEffect(() => {
+        document.body.addEventListener('dragenter', enter)
+        document.body.addEventListener('dragover', over)
+        document.body.addEventListener('drop', drop)
+        return () => {
+            document.body.removeEventListener('dragenter', enter)
+            document.body.removeEventListener('dragover', over)
+            document.body.removeEventListener('drop', drop)
+        }
+    })
+    return null
 }
