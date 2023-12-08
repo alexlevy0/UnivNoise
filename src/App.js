166fc2d92dde362cdb0712a4996951fb1089af20
Alex Lévy <alexlevy0@gmail.com>
2023-12-08 05:10:22 +0100

    vercel stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { OrbitControls, CameraShake } from '@react-three/drei'
 import { useControls } from 'leva'
 import { Particles } from './Particles'
+import { SpeedInsights } from '@vercel/speed-insights/react'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App() {
   const props = useControls({
@@ -8,13 +10,28 @@ export default function App() {
     speed: { value: 100, min: 0.1, max: 100, step: 0.1 },
     aperture: { value: 1, min: 1, max: 5.6, step: 0.1 },
     fov: { value: 50, min: 0, max: 200 },
-    curl: { value: 0.25, min: 0.01, max: 0.5, step: 0.01 }
+    curl: { value: 0.25, min: 0.01, max: 0.5, step: 0.01 },
   })
   return (
     <>
-      <OrbitControls makeDefault autoRotate autoRotateSpeed={0.5} zoomSpeed={0.1} />
-      <CameraShake yawFrequency={1} maxYaw={0.05} pitchFrequency={1} maxPitch={0.05} rollFrequency={0.5} maxRoll={0.5} intensity={0.2} />
+      <OrbitControls
+        makeDefault
+        autoRotate
+        autoRotateSpeed={0.5}
+        zoomSpeed={0.1}
+      />
+      <CameraShake
+        yawFrequency={1}
+        maxYaw={0.05}
+        pitchFrequency={1}
+        maxPitch={0.05}
+        rollFrequency={0.5}
+        maxRoll={0.5}
+        intensity={0.2}
+      />
       <Particles {...props} />
+      <SpeedInsights />
+      <Analytics />
     </>
   )
 }
